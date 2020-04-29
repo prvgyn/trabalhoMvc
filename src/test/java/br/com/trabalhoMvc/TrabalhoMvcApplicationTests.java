6b4867da2ffcf2efef8ee00df61df278bb86f6d6
BrMorato <henriquemorato@yahoo.com.br>
2020-04-29 16:23:51 -0300

    Initial commit

---
+++ src/test/java/br/com/trabalhoMvc/TrabalhoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalhoMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
